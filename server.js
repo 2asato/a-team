5e3bb9ea5d388ecb080195f1b35a85adbad8e179
aaronsato <2asato@gmail.com>
2017-08-21 20:39:36 -0700

    seed routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 
@@ -9,6 +10,17 @@ app.use('/characters', charactersController);
 
 const episodesController = require('./controllers/episodes.js');
 app.use('/episodes', episodesController);
+
+const characterseedController = require('./models/characterseed')
+app.use('/seedcharacters', characterseedController);
+
+const episodeseedController = require('./models/episodeseed')
+app.use('/seedepisodes', episodeseedController);
+
+mongoose.connect('mongodb://localhost:27017/ateam')
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+})
 
 app.listen(3000, function(){
     console.log('listening...');
